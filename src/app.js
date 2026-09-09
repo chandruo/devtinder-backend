bea0082bc6fa68d981910d79863160f56f5819f0
siva <chandru04042000@gmail.com>
2026-09-09 17:44:11 +0530

    tested all user APIs

--- src/app.js
+++ src/app.js
@@ -1,68 +1,98 @@
-const express = require("express")
-const connectDB = require("./config/database")
-const User = require("./models/user")
-const app = express()
+const express = require("express");
+const connectDB = require("./config/database");
+const User = require("./models/user");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-
-connectDB().then(() => {
-  console.log("connection successful")
-  app.listen(3000, () => {
-    console.log("server started running")
+connectDB()
+  .then(() => {
+    console.log("connection successful");
+    app.listen(777, () => {
+      console.log("server started running");
+    });
   })
-}).catch(err => console.log(err))
-
+  .catch((err) => console.log(err));
 
 app.post("/signup", async (req, res) => {
-  const user = req.body
-  await user.save()
-  res.send("sign up api")
-})
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.status(201).json({
+      message: "user created successfully",
+      user,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "something went wrong",
+      error: err,
+    });
+  }
+});
 
-
-app.get("/user", async (req, res) => {
-  const { userId } = req.body
+app.get("/user/:id", async (req, res) => {
+  const { id } = req.params;
 
   try {
-    const user = await User.find({ emailId: userId })
+    const user = await User.findById(id);
 
-    if (user.length) {
-      res.send(user)
-    } else {
-      res.send("user not found")
+    if (!user) {
+      return res.status(404).json({
+        message: "user not found",
+      });
     }
+    res.status(200).json({
+      message: "user found",
+      user,
+    });
   } catch (err) {
-    res.status(500).send("something went wrong")
+    res.status(500).json({ message: "something went wrong", error: err });
   }
-
-})
+});
 
 app.get("/feed", async (req, res) => {
   try {
-    const users = User.find({})
-    if (users.length) {
-      res.send(users)
-    } else {
-      res.send("No feed found")
+    const users = await User.find({});
+
+    if (users.length === 0) {
+      return res.status(404).json({
+        message: "No feed found",
+      });
     }
 
+    res.status(200).json({ users });
   } catch (err) {
-    res.status(500).send("something went wrong")
+    res.status(500).send("something went wrong");
   }
-})
+});
 
-app.delete("/user", async (req, res) => {
-  const { userId } = req.body
-  await User.findByIdAndDelete(userId)
+app.delete("/user/:id", async (req, res) => {
+  const { id } = req.params;
 
-  res.send("user deleted successfully")
-
-})
+  try {
+    const user = await User.findByIdAndDelete(id);
+    if (!user) {
+      return res.status(400).json({ message: "No user found" });
+    }
+    res.status(200).json({
+      message: "user deleted successfully",
+    });
+  } catch (err) {
+    res.status(500).json("something wrong");
+  }
+});
 
 app.patch("/user", async (req, res) => {
-  const data = req.body
-  const user = await User.findByIdAndUpdate(data.id, data)
-  console.log(user)
-  res.send("updated successfully")
-})
+  const { userId, ...data } = req.body;
+  try {
+    const user = await User.findByIdAndUpdate(userId, data, { new: false });
+    if (!user) {
+      res.status(400).json({ message: "update failed" });
+    }
+    res.send("User details updated successfully");
+  } catch (err) {
+    res.status(500).json({
+      message: "something went wrong",
+    });
+  }
+});
